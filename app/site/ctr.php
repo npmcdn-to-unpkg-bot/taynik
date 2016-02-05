b6a7434627db7b7dd22e6fc509abdfe39602bec5
neuromax <maxelltmg85@gmail.com>
2016-02-05 21:12:03 +0600

    Доработка шаблона

--- app/site/ctr.php
+++ app/site/ctr.php
@@ -9,6 +9,13 @@ class Ctr extends Controller {
     function index ()
     {
 
+        if ($_SERVER['REQUEST_METHOD'] == "POST")
+        {
+
+            var_dump($_POST);
+
+        }
+
         $content = \app\site\modules\card\Controller::showListCard();
 
         $this->view->generate(THEME . 'template.html', $content, $this->data);
